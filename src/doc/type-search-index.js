fee36734855b4524aca87e27ef738b7863466715
Jonatas <ariane.etec.inf@gmail.com>
2021-08-12 15:54:01 -0300

    first commit

---
+++ src/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"br.com.dio.model","l":"Gato1"}];updateSearchResults();
---
+++ src/src/com/dio/PrimeiroPgr.java
@@ -0,0 +1,26 @@
+package com.dio;
+
+import br.com.dio.model.Gato1;
+
+public class PrimeiroPgr {
+
+	public static void main(String[] args) {
+		
+		Gato1 Gato1 = new Gato1();
+		Livros Livros = new Livros();
+		
+		System.out.println(Gato1);
+		System.out.println(Livros);
+		
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello word!" + (a+b));*/
+
+	}
+
+}
+ class Livros {
+	 private String nome;
+	 private String npag;
+	 
+ }
